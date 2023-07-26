2948eb0779b19a573e6847cb71c9abcca73c951f
SharonMai <ppp300a@gmail.com>
2023-07-26 13:51:08 +0800

    test.yml delete flower

--- prj_dept/celery.py
+++ prj_dept/celery.py
@@ -1,11 +1,11 @@
 import os
 from celery import Celery
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prj_dept.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 
 app = Celery('prj_dept')
 
-app.config_from_object('settings', namespace='CELERY')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
