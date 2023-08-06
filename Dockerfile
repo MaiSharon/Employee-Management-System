5fdcc4742e9049814ef2fed64eb047b40237501d
SharonMai <ppp300a@gmail.com>
2023-08-06 21:39:30 +0800

    add uwsgi and test docker run

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,20 @@ FROM python:3.9-alpine
 WORKDIR /data/prj_dept
 ENV server_params=
 COPY requirements.txt ./
+
+# 安裝必要的包和庫，然後清理
 RUN apk update && \
     apk add --no-cache gcc musl-dev libffi-dev mariadb-connector-c-dev gettext && \
     python3 -m pip install --upgrade pip && \
     pip install -r requirements.txt && \
+    pip install uwsgi && \
     apk del gcc musl-dev libffi-dev && \
-    rm -rf /var/cache/apk/*
-
-RUN ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
+    rm -rf /var/cache/apk/* && \
+    ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
     echo 'Asia/Taipei' >/etc/timezone
-
-# install uWSGI
-RUN pip install uwsgi
 
 # 將當前目錄（即 Dockerfile 所在的目錄）下的所有文件和子目錄複製到 WORKDIR 路徑中
 COPY . .
 
-RUN chmod +x ./start.local.sh
+RUN chmod +x ./start.prod.sh
 EXPOSE 8000
-# CMD ["/bin/sh", "/data/prj_dept/start.local.sh"]
---
+++ start.prod.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# django-admin compilemessages
+django-admin compilemessages
+
+# if local config file does not exist, clone one:
+if [ ! -f "settings/local.py" ]; then
+    echo "=== warning: local.py does not exist, will initialize the file, please update the configs ==="
+    cp settings/production.py settings/local.py
+    sed -i 's/DEBUG = False/DEBUG = False/g' settings/local.py
+fi
+
+# Try to connect to database using Django's manage.py shell
+echo "=== Attempting to connect to the database ==="
+until echo "from dept_app.models import Admin; print(Admin.objects.count())" | python manage.py shell $server_params ; do
+    echo "Waiting for the database to start"
+    sleep 2
+done
+echo "Database connection successful"
+
+# Collect static files (--noinput ->It's say yes)
+echo "=== Collecting static files ==="
+python manage.py collectstatic --noinput $server_params
+
+# 使用 uWSGI 運行 Django 應用
+exec uwsgi --ini /data/prj_dept/uwsgi.ini
