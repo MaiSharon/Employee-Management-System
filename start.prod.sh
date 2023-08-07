0140124034362e12ed80600739cc02fbea0949a0
SharonMai <ppp300a@gmail.com>
2023-08-07 15:33:34 +0800

    start is root end is uwsgiuser

--- start.prod.sh
+++ start.prod.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# 更改靜態文件的權限
+chown -R uwsgiuser:uwsgi /data/prj_dept/staticfiles/
+
 # django-admin compilemessages
 django-admin compilemessages
 
@@ -26,7 +29,6 @@ echo "Database connection successful"
 echo "=== Collecting static files ==="
 python manage.py collectstatic --noinput $server_params
 
-RUN chown -R uwsgiuser:uwsgi /data/prj_dept/staticfiles/
-
 # 使用 uWSGI 運行 Django 應用
-exec uwsgi --ini /data/prj_dept/uwsgi.ini
+# 使用 su 命令以 uwsgiuser 身份運行 uwsgi
+su uwsgiuser -c "exec uwsgi --ini /data/prj_dept/uwsgi.ini"
