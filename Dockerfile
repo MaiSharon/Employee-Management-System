53397326a7a3b85f60d220364d3d6184ce5908bf
SharonMai <ppp300a@gmail.com>
2023-08-15 21:10:06 +0800

    fix ownership in test.yml

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
+# 使用 Python 3.9 的 Alpine 版本作為基本映像
 FROM python:3.9-alpine
+
+# 設置工作目錄
 WORKDIR /data/prj_dept
+
+# 設置環境變量
 ENV server_params=
+
+# 複製 requirements 文件
 COPY requirements.txt ./
 
-# 創建非root用戶和組
+# 創建非 root 用戶和組
 RUN addgroup -S uwsgi && adduser -S uwsgiuser -G uwsgi
-
 
 # 安裝必要的包和庫，然後清理
 RUN apk update && \
@@ -18,22 +24,21 @@ RUN apk update && \
     ln -sf /usr/share/zoneinfo/Asia/Taipei /etc/localtime && \
     echo 'Asia/Taipei' >/etc/timezone
 
-# 將當前目錄（即 Dockerfile 所在的目錄）下的所有文件和子目錄複製到 WORKDIR 路徑中
+# 複製當前目錄的所有文件和子目錄到工作目錄
 COPY . .
+
 # 設定日誌文件和靜態文件的權限
 RUN touch /data/prj_dept/dept_app.performance.log && \
     touch /data/prj_dept/dept_app.task.log && \
     touch /data/prj_dept/dept_app.log && \
-    chown uwsgiuser:uwsgi /data/prj_dept/dept_app.performance.log && \
-    chown uwsgiuser:uwsgi /data/prj_dept/dept_app.task.log && \
-    chown uwsgiuser:uwsgi /data/prj_dept/dept_app.log
+    chown uwsgiuser:uwsgi /data/prj_dept/dept_app.*.log
 
-# Change ownership of the entire project directory
-# RUN chown -R uwsgiuser:uwsgi /data/prj_dept/
-# RUN rm -rf /data/prj_dept/staticfiles/*
-
+# 設置執行權限
 RUN chmod +x ./start.prod.sh
+
+# 暴露端口
 EXPOSE 8000
 
-# 使用非root用戶運行容器
+# 使用非 root 用戶運行容器
 USER uwsgiuser
+
--- start.prod.sh
+++ start.prod.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-# django-admin compilemessages
+# 編譯 Django 消息
 django-admin compilemessages
 
-# if local config file does not exist, clone one:
+# 如果本地配置文件不存在，則複製一個
 if [ ! -f "settings/local.py" ]; then
     echo "=== warning: local.py does not exist, will initialize the file, please update the configs ==="
     cp settings/production.py settings/local.py
     sed -i 's/DEBUG = False/DEBUG = False/g' settings/local.py
 fi
 
-# Try to connect to database using Django's manage.py shell
+# 嘗試使用 Django 的 manage.py shell 連接到數據庫
 echo "=== Attempting to connect to the database ==="
 until echo "from dept_app.models import Admin; print(Admin.objects.count())" | python manage.py shell; do
     echo "Waiting for the database to start"
@@ -19,9 +19,13 @@ until echo "from dept_app.models import Admin; print(Admin.objects.count())" | p
 done
 echo "Database connection successful"
 
-# Collect static files (--noinput ->It's say yes)
+# 收集靜態文件
 echo "=== Collecting static files ==="
 python manage.py collectstatic --noinput
+
+# 更改靜態文件的所有權
+echo "=== Changing ownership of static files ==="
+chown -R www-data:www-data /data/prj_dept/staticfiles
 
 # 使用 uWSGI 運行 Django 應用
 exec uwsgi --ini /data/prj_dept/uwsgi.ini
