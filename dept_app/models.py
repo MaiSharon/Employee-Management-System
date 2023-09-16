a43134f5caec341b6abe0291dd61119df963fd76
SharonMai <ppp300a@gmail.com>
2023-09-17 07:53:54 +0800

    🛠️🗃️refactor(models.UserInfo and MobileNum and migration files): Refactor models and generate migration files
    - Removed 'level' field from PrettyNum model.
    - Removed 'account' and
    'password' fields from UserInfo model.
    - Added 'brand' field to
    PrettyNum with choices for iPhone models.
    - Added 'quota' field to
    UserInfo for educational allowance in USD.
    - Renamed PrettyNum model to
    MobileNum.
    -
    Updated 'price' and 'status' fields in PrettyNum (now MobileNum) model.
    
    BREAKING CHANGE: Yes, these changes require running new migrations,
    which will alter the database schema and potentially break existing
    functionality.

--- dept_app/models.py
+++ dept_app/models.py
@@ -45,7 +45,6 @@ class Department(models.Model):
 class UserInfo(models.Model):
     """員工表"""
     name = models.CharField(verbose_name="姓名", max_length=16)
-    password = models.CharField(verbose_name="密碼", max_length=64)
     birthday = models.DateField(verbose_name="生日", default='2000-01-01')
 
     def age(self):
@@ -54,53 +53,39 @@ class UserInfo(models.Model):
         today = date.today()
         return today.year - self.birthday.year - ((today.month, today.day) < (self.birthday.month, self.birthday.day))
 
-    account = models.DecimalField(verbose_name="帳號餘額", max_digits=10, decimal_places=2, default=0)
+    quota = models.DecimalField(verbose_name="進修額度(美金)", max_digits=10, decimal_places=2, default=0)
 
-    ### 相同變量名字更該數據類型不影響數據
-    # create_time = models.DateTimeField(verbose_name="就職時間")
     create_time = models.DateField(verbose_name="就職時間") # 只有年月日沒有時分秒
 
-    # 數據庫中的命令
-    # 1.有約束
-    #   to  與哪一張表關聯
-    #   to_field, 表中的哪一列關聯
-    # 2.django自動生成
-    #   寫的depart會生成數據列後面加上_id 變成 depart_id
-    # 3.部門表被刪除
-    # ###3.1 聯級(被約束的一同刪除)刪除
-    # depart = models.ForeignKey(to="Department", to_field="id", on_delete=models.CASCADE)
-    # ###3.2 置空(有聯級被刪除後就空著)
     depart = models.ForeignKey(verbose_name="部門", to="Department", to_field="id", null=True, blank=True, on_delete=models.SET_NULL)
 
-    # 在django中的命令
-    # 加上choices 只能寫元祖中已有的內容
     gender_choices = (
         (1, "男"),
         (2, "女"),
     )
-    # gender = models.SmallIntegerField(verbose_name="性別", choices=gender_choices, unique=True, null=True)
+
     gender = models.SmallIntegerField(verbose_name="性別", choices=gender_choices)
 
 
-class PrettyNum(models.Model):
+class MobileNum(models.Model):
     # 手機號碼用字符串原因是方便搜尋
     mobile = models.CharField(verbose_name="手機號碼", max_length=11)
-    price = models.IntegerField(verbose_name="費用", default=0)
+    price = models.IntegerField(verbose_name="電信費", default=0)
 
-    level_choices = (
-        (1, "一級"),
-        (2, "二級"),
-        (3, "三級"),
+    brand_choices = (
+        (1, "iPhone 15"),
+        (2, "iPhone 15 Pro"),
+        (3, "iPhone 14"),
     )
 
-    level = models.SmallIntegerField(verbose_name="級別", choices=level_choices)
+    brand = models.SmallIntegerField(verbose_name="品牌", choices=brand_choices)
 
     status_choices = (
-        (1, "已佔用"),
-        (2, "未占用"),
+        (1, "已出借"),
+        (2, "未出借"),
     )
 
-    status = models.SmallIntegerField(verbose_name="占用", choices=status_choices, default=2)
+    status = models.SmallIntegerField(verbose_name="出借", choices=status_choices, default=2)
 
 
 class Task(models.Model):
