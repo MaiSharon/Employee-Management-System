53b4529d7e6d43cd0a99e3d8fb98aa3af6fbbafb
SharonMai <ppp300a@gmail.com>
2023-09-18 00:15:42 +0800

    ✨feat(views/user.py(clear_cache function)): Add {"page_title": "title name"} variable for frontend template rendering and remove redis cache

--- dept_app/views/user.py
+++ dept_app/views/user.py
@@ -1,19 +1,13 @@
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
-from django.core.cache import cache
+
 from django.shortcuts import render, redirect
-from django.views.decorators.cache import cache_page
+
 
 from dept_app import models
 from dept_app.utils.pagination import Pagination
 from dept_app.utils.form import UserModelForm
 # from django.core import paginator
 
-def clear_cache(sender, **kwargs):
-    print("hi im chche single")
-    cache.delete('cache_user_list')
-    updated_data = models.UserInfo.objects.all()
-    cache.set('cache_user_list', updated_data, 60 * 15)
+
 
 def get_filitered_userinfo(search):
     """跟玉搜索條件返回過濾後的UserInfo數據"""
@@ -26,24 +20,11 @@ def get_filitered_userinfo(search):
 
 def user_list(request):
 
-
     # step1:分析搜尋條件
     search_query = request.GET.get("search", "")  # 後面""為預設空字串，讓input框內不出現None字符
 
     # # step2:依據搜尋條件獲取數據
     filtered_userinfo = get_filitered_userinfo(search_query)
-
-
-    # # For each distinct search_query, store and retrieve its own cache
-    # cache_key = f'cache_user_list:{search_query}'
-    #
-    # # Try to get filtered userinfo from cache
-    # filtered_userinfo = cache.get(cache_key)
-    # print(filtered_userinfo)
-    # # If cache does not exist, query the data and set the cache
-    # if filtered_userinfo is None:
-    #     filtered_userinfo = get_filitered_userinfo(search_query)
-    #     cache.set('cache_user_list', filtered_userinfo, 60 * 15)
 
     # step3:分頁處理
     paginator = Pagination(request,filtered_userinfo, page_size=5)
@@ -52,6 +33,7 @@ def user_list(request):
         "search": search_query,
         "queryset": paginator.page_queryset,  # 分完頁的數據
         "page_string": paginator.html(),  # 頁碼
+        "page_title": "Employees"
 
     }
 
@@ -62,17 +44,15 @@ def user_list(request):
 def user_add(request):
     if request.method == "GET":
         form = UserModelForm()
-        return render(request, "user_add.html", {'form': form})
+        return render(request, "user_add.html", {"form": form, "page_title": "Employee Add"})
 
     form = UserModelForm(data=request.POST)
     if form.is_valid():
         form.save()
-        print("hi ,im single add")
         return redirect("/user/list/")
-    return render(request, "user_add.html", {"form": form})
+    return render(request, "user_add.html", {"form": form, "page_title": "Employee Add"})
 
 
-# TODO need fix redis
 def user_edit(request, nid):
     """編輯用戶"""
     row_object = models.UserInfo.objects.filter(id=nid).first()
@@ -80,15 +60,15 @@ def user_edit(request, nid):
     if request.method == "GET":
         # instance默認把每一個數值在欄位中顯示出來，等同於input的value值
         form = UserModelForm(instance=row_object)
-        return render(request, "user_edit.html", {'form': form})
+        return render(request, "user_edit.html", {'form': form, "page_title": "Employee Edit"})
 
     # instance等於是把前面data數據指定更新到這instance這一行
     form = UserModelForm(data=request.POST, instance=row_object)
     if form.is_valid():
         form.save()
-        cache.delete('cache_user_list')
+
         return redirect("/user/list/")
-    return render(request, "user_edit.html", {'form': form})
+    return render(request, "user_edit.html", {'form': form, "page_title": "Employee Edit"})
 
 
 def user_delete(request, nid):
