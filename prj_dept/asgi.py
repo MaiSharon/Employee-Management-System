ad09c44adba86675f12bb3d44a1ce7a50535c336
SharonMai <ppp300a@gmail.com>
2023-09-27 23:42:53 +0800

    :sparkles: feature(prj_dept/asgi.py): Add CustomAuthMiddleware to WebSocket routing

--- prj_dept/asgi.py
+++ prj_dept/asgi.py
@@ -1,31 +1,21 @@
-"""
-ASGI config for prj_dept project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
-"""
 import os
-
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from . import routing
-
-
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from . import routing
+from prj_dept.middleware_for_asgi import CustomAuthMiddleware  # 確保這個import路徑是正確的
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prj_dept.setting')
 
 django_asgi_app = get_asgi_application()
 
-# support HTTP and WebSocket
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,  # automatic find urls.py and find views function ==> HTTP
-    "websocket": AuthMiddlewareStack(  # routings(urls)、 consumers(views)
-        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    "http": django_asgi_app,
+    "websocket": CustomAuthMiddleware(
+        AuthMiddlewareStack(
+            URLRouter(
+                routing.websocket_urlpatterns
+            )
+        )
     ),
-}
-)
+})
