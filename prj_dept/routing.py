b5a1d95152bbcc43239ae2d182d9af63524e393a
SharonMai <ppp300a@gmail.com>
2023-09-27 23:43:25 +0800

    :sparkles: feature(prj_dept/routing.py): Implement WebSocket path for chat room

--- prj_dept/routing.py
+++ prj_dept/routing.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
+from django.urls import path
 from dept_app import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    path("ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
 ]
