6e3eaac46729a72ed0f7e07c4e9617f372e504eb
SharonMai <ppp300a@gmail.com>
2023-09-27 23:48:20 +0800

    :sparkles: feature(settings/base.py): Uncomment and enable custom authentication middleware
    Closes No

--- settings/base.py
+++ settings/base.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'dept_app.middleware.auth.AuthMiddleware',
+    'dept_app.middleware.auth.AuthMiddleware',
 ]
 INTERNAL_IPS = [
     # ...
