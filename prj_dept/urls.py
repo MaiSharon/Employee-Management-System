487f64df20c0efb24e553d9846a45c46bb6c955f
SharonMai <ppp300a@gmail.com>
2023-09-30 00:01:12 +0800

    :wrench: refactor(urls.py): Add path url name

--- prj_dept/urls.py
+++ prj_dept/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from dept_app.views import depart, mobile, user, admin, login, task, register
 from django.views.generic import TemplateView
-from django.urls import path
+
+from dept_app.views import depart, mobile, user, admin, login, task, register,chat
+
 
 def trigger_error(request):
   division_by_zero = 1 / 0
@@ -13,43 +14,40 @@ def trigger_error(request):
   #   # ...
   # ]
 urlpatterns = [
-
     path('about/', TemplateView.as_view(template_name="no_js.html")),
+
+    # asgi test
+    path("chat/", chat.index, name="index"),
+    path("chat/<str:room_name>/", chat.room, name="room"),
 
     # path('sentry-debug/', trigger_error),
     # path('admin/', admin.site.urls),
-    path('depart/list/', depart.depart_list),
-    path('depart/add/', depart.depart_add),
-    path('depart/<int:nid>/edit/', depart.depart_edit),
-    path('depart/<int:nid>/delete/', depart.depart_delete),
+    path('depart/list/', depart.depart_list, name='depart_list'),
+    path('depart/add/', depart.depart_add, name='depart_add'),
+    path('depart/<int:nid>/edit/', depart.depart_edit, name='depart_edit'),
+    path('depart/<int:nid>/delete/', depart.depart_delete, name='depart_delete'),
 
-    path('user/list/', user.user_list),
-    path('user/add/', user.user_add),
-    path('user/<int:nid>/edit/', user.user_edit),
-    path('user/<int:nid>/delete/', user.user_delete),
+    path('user/list/', user.user_list, name='user_list'),
+    path('user/add/', user.user_add, name='user_add'),
+    path('user/<int:nid>/edit/', user.user_edit, name='user_edit'),
+    path('user/<int:nid>/delete/', user.user_delete, name='user_delete'),
 
-    path('mobile/list/', mobile.mobile_list),
-    path('mobile/add/', mobile.mobile_add),
-    path('mobile/<int:nid>/edit/', mobile.mobile_edit),
-    path('mobile/<int:nid>/delete/', mobile.mobile_delete),
+    path('mobile/list/', mobile.mobile_list, name='mobile_list'),
+    path('mobile/add/', mobile.mobile_add, name='mobile_add'),
+    path('mobile/<int:nid>/edit/', mobile.mobile_edit, name='mobile_edit'),
+    path('mobile/<int:nid>/delete/', mobile.mobile_delete, name='mobile_delete'),
 
     path('admin/list/', admin.admin_list, name="admin_list"),
-    path('admin/add/', admin.admin_add, name="admin_add"),
     path('admin/<int:nid>/edit/', admin.admin_edit, name="admin_edit"),
-    path('admin/<int:nid>/delete/', admin.admin_delete, name="admin_delete"),
     path('admin/<int:nid>/reset/', admin.admin_reset, name="admin_reset"),
 
-    # path('image/add/', image.image_add),
-    # path('image/<int:nid>/delete/', image.image_delete),
-
-
-    path('register/', register.admin_add, name="register"),
+    path('register/', register.register, name="register"),
     path('verify/<str:token>/', register.verify_email, name='verify_email'),
     path('re-verify/', register.re_verify, name='re_verify'),
 
     path('login/', login.login, name="login"),
-    path('logout/', login.logout),
-    path('image/code/', login.image_code),
+    path('logout/', login.logout, name="logout"),
+    path('image/code/', login.image_code, name="image_code"),
 
     path('task/', task.task_list),
     path('task/ajax/', task.task_sayhi),
