f9331ea6cd2604533e38cc17ddc2b15e81d3b459
SharonMai <ppp300a@gmail.com>
2023-09-30 23:53:07 +0800

    :wrench: refactor: Replace 'user' to 'employee' and 'html' to 'generate_html'
    - Change better reflect the business context.

--- dept_app/utils/pagination.py
+++ dept_app/utils/pagination.py
@@ -1,151 +1,99 @@
-"""
-自定義的分頁組件
-
-在視圖函數中:
-    def mobile_list(request):
-
-        # 1.根據自己的情況去篩選自己的數據
-        total_count = models.mobileNum.objects.all()
-
-        # 2.實力化分頁對象
-        page_object = Pagination(request, queryset)
-
-        context = {
-        "search": search,
-
-        "queryset": page_object.page_queryset,  # 分完頁的數據
-        "page_string": page_object.html()  # 頁碼
-        }
-
-在html中
-    {% for item in queryset%}
-        <td scope="row">{{ item.id }}</td>
-        <td>{{ item.mobile }}</td>
-        <td>{{ item.price }}</td>
-        <td>{{ item.get_level_display }}</td>
-        <td>{{ item.get_status_display }}</td>
-    {% endfor %}
-
-    <ul class="pagination">
-        {{page_string}}
-    </ul>
-
-"""
 from django.utils.safestring import mark_safe
-
 
 class Pagination(object):
 
-    def __init__(self, request, queryset, page_size=10, page_param="page", plus=5):
+    def __init__(self, request, queryset, page_size=5, page_param="page", plus=5):
         """
-        :param request: 請求的對象
-        :param queryset:  符合條件的數據(根據這個數據給他進行分頁處理)
-        :param page_size: 每頁顯示多少條數據
-        :param page_param: 在URL中傳遞的獲取分頁的參數 例如: /mobile/list/?page=12
-        :param plus: 顯示當前頁的 前或後幾頁(頁碼)
+        初始化分頁對象。
+
+        Args:
+            request: HttpRequest對象，網絡請求對象。
+            queryset: QuerySet，要分頁的查詢集。
+            page_size: int, 可選，每頁包含的項目數。
+            page_param: str, 可選，查詢字符串中頁面參數的名稱。
+            plus: int, 可選，當前頁面前後要顯示的頁數。
         """
         from django.http.request import QueryDict
         import copy
+
+        # 深度複製請求參數並使其可變
         query_dict = copy.deepcopy(request.GET)
         query_dict._mutable = True
         self.query_dict = query_dict
-        # query_dict.setlist('xx', [11])
-
         self.page_param = page_param
-        page = request.GET.get(page_param, "1")  # 獲取當前頁
-
         self.page_size = page_size
 
+        # 獲取當前頁數，默認為第一頁
+        page = request.GET.get(page_param, "1")
+
+        # 計算總頁數
         total_count = queryset.count()
-        total_page, page_other = divmod(total_count, page_size)  # 取得總頁碼數。取整數(商數)->總頁碼數、餘數->多出的數據也成一頁
-        if page_other:  # 如果餘數有數值為True也就是無法滿足page_size為完整一頁時，但數據也是要呈現出來
+        total_page, remainder = divmod(total_count, page_size)
+        if remainder:
             total_page += 1
         self.total_page_count = total_page
 
-        # --------頁碼搜尋不超過目前最大頁碼---------
-
-        if page.isdecimal():  # 判斷字串是否只有十進製的數字
-            page = int(page)  # 確定只有字串中只有數字才進行數據類型轉換成整型
+        # 確保當前頁數在有效範圍內
+        if page.isdecimal():
+            page = int(page)
             if page > self.total_page_count:
                 page = self.total_page_count
         else:
-            page = 1  # 字串中不是只有數字則默認等於1
+            page = 1
         self.page = page
-        # print(self.page)
 
-        self.start = max(0, (page - 1) * page_size)  # 才會符合切片從0開始計算
-        self.end = max(0, page * page_size)
+        # 計算當前頁的資料範圍
+        self.start = max(0, (page - 1) * page_size)
+        self.end = self.start + page_size
 
+        # 取出當前頁的數據
         self.page_queryset = queryset[self.start:self.end]
 
-        self.plus = plus  # 決定當前位置的前後各出現幾頁
+        # 設置顯示的頁碼範圍
+        self.plus = plus
 
-    def html(self):
-        # 數據庫中的數據比較少，都沒有達到11頁
-        if self.total_page_count <= self.plus * 2 + 1:  # 11 ，如果之後網頁呈現頁碼數量要改的話，不用手動改此，改plus即可
-            start_page = 1
-            end_page = self.total_page_count
+    def generate_html(self):
+        """
+        生成 HTML 分頁控制元件。
 
-        # 8.2 數據庫中數據大於11頁
-        else:
-            # 8.3 當前頁<5
-            if self.page <= self.plus:  # 判斷當前頁碼
-                start_page = 1
-                end_page = self.plus * 2 + 1
-
-            # 8.4 當前頁+5 >總頁碼
-            else:
-                start_page = self.page - self.plus
-                if (self.page + self.plus) > self.total_page_count:  # 判斷頁碼是否超出數據
-                    end_page = self.total_page_count
-                else:
-                    end_page = self.page + self.plus
-
+        Returns:
+            str: HTML 分頁控件
+        """
         page_str_list = []
-        n = "\n"
-        self.query_dict.setlist(self.page_param, [1])  # "page": 1
-        self.query_dict.urlencode()  # page=1
 
-        # 10. 頁碼首頁
-        # home_page = f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">首頁</a></li>'
-        # page_str_list.append(home_page)
-
-        # 9. 上一頁
+        # 處理「上一頁」
         if self.page > 1:
             self.query_dict.setlist(self.page_param, [self.page - 1])
-            prev_page = f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">Previous</a></li>'
         else:
             self.query_dict.setlist(self.page_param, [1])
-            prev_page = f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">Previous</a></li>'
+        prev_page = f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">Previous</a></li>'
         page_str_list.append(prev_page)
 
-        # 5. 生成頁碼，做成列表且去除換行
-        for i in range(start_page, end_page + 1):  # range是前取後不取因此記得要在原本數值上+1
-            self.query_dict.setlist(self.page_param, [i])
-            # 實現當前選取頁面 html裡面加上 active
-            if i == self.page:
-                page_list = f'<li class="page-item active" aria-current="page"><a class="page-link" href="?{self.query_dict.urlencode()}">{i}</a></li>'
+        # 計算要顯示的頁碼範圍
+        if self.total_page_count <= self.plus * 2 + 1:
+            start_page, end_page = 1, self.total_page_count
+        else:
+            if self.page <= self.plus:
+                start_page, end_page = 1, self.plus * 2 + 1
             else:
-                page_list = f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">{i}</a></li>'
-            page_str_list.append(page_list)
+                start_page = self.page - self.plus
+                end_page = min(self.total_page_count, self.page + self.plus)
 
-        # 9.1 下一頁
+        # 生成頁碼列表
+        for i in range(start_page, end_page + 1):
+            self.query_dict.setlist(self.page_param, [i])
+            if i == self.page:
+                page_str_list.append(f'<li class="page-item active" aria-current="page"><a class="page-link" href="?{self.query_dict.urlencode()}">{i}</a></li>')
+            else:
+                page_str_list.append(f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">{i}</a></li>')
+
+        # 處理「下一頁」
         if self.page < self.total_page_count:
             self.query_dict.setlist(self.page_param, [self.page + 1])
-            next_page = f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">Next</a></li>'
         else:
             self.query_dict.setlist(self.page_param, [self.total_page_count])
-            next_page = f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">Next</a></li>'
+        next_page = f'<li class="page-item"><a class="page-link" href="?{self.query_dict.urlencode()}">Next</a></li>'
         page_str_list.append(next_page)
 
-        page_string = mark_safe(f'{"".join(page_str_list)}{n}')
-
-        return page_string
-
-
-
-
-
-
-
-
+        # 返回生成的HTML
+        return mark_safe(''.join(page_str_list))
--- dept_app/views/employee.py
+++ dept_app/views/employee.py
@@ -1,16 +1,12 @@
-
 from django.shortcuts import render, redirect
-
 
 from dept_app import models
 from dept_app.utils.pagination import Pagination
-from dept_app.utils.form import UserModelForm
-# from django.core import paginator
-
+from dept_app.utils.form import EmployeeModelForm
 
 
 def get_filitered_userinfo(search):
-    """跟玉搜索條件返回過濾後的UserInfo數據"""
+    """返回過濾後的UserInfo數據"""
     if not search:
         return models.UserInfo.objects.all()
     else:
@@ -18,12 +14,12 @@ def get_filitered_userinfo(search):
         return models.UserInfo.objects.filter(**search_data)
 
 
-def user_list(request):
+def employee_list(request):
 
     # step1:分析搜尋條件
     search_query = request.GET.get("search", "")  # 後面""為預設空字串，讓input框內不出現None字符
 
-    # # step2:依據搜尋條件獲取數據
+    # step2:依據搜尋條件獲取數據
     filtered_userinfo = get_filitered_userinfo(search_query)
 
     # step3:分頁處理
@@ -32,46 +28,45 @@ def user_list(request):
     context = {
         "search": search_query,
         "queryset": paginator.page_queryset,  # 分完頁的數據
-        "page_string": paginator.html(),  # 頁碼
+        "page_string": paginator.generate_html(),  # 頁碼
         "page_title": "Employees"
 
     }
 
-    return render(request, 'user_list.html', context)
+    return render(request, 'employee_list.html', context)
 
 
-
-def user_add(request):
+def employee_add(request):
     if request.method == "GET":
-        form = UserModelForm()
-        return render(request, "user_add.html", {"form": form, "page_title": "Employee Add"})
+        form = EmployeeModelForm()
+        return render(request, "employee_add.html", {"form": form, "page_title": "Employee Add"})
 
-    form = UserModelForm(data=request.POST)
+    form = EmployeeModelForm(data=request.POST)
     if form.is_valid():
         form.save()
         return redirect("/user/list/")
-    return render(request, "user_add.html", {"form": form, "page_title": "Employee Add"})
+    return render(request, "employee_add.html", {"form": form, "page_title": "Employee Add"})
 
 
-def user_edit(request, nid):
+def employee_edit(request, nid):
     """編輯用戶"""
     row_object = models.UserInfo.objects.filter(id=nid).first()
 
     if request.method == "GET":
         # instance默認把每一個數值在欄位中顯示出來，等同於input的value值
-        form = UserModelForm(instance=row_object)
-        return render(request, "user_edit.html", {'form': form, "page_title": "Employee Edit"})
+        form = EmployeeModelForm(instance=row_object)
+        return render(request, "employee_edit.html", {'form': form, "page_title": "Employee Edit"})
 
     # instance等於是把前面data數據指定更新到這instance這一行
-    form = UserModelForm(data=request.POST, instance=row_object)
+    form = EmployeeModelForm(data=request.POST, instance=row_object)
     if form.is_valid():
         form.save()
 
         return redirect("/user/list/")
-    return render(request, "user_edit.html", {'form': form, "page_title": "Employee Edit"})
+    return render(request, "employee_edit.html", {'form': form, "page_title": "Employee Edit"})
 
 
-def user_delete(request, nid):
+def employee_delete(request, nid):
     """刪除用戶"""
     models.UserInfo.objects.filter(id=nid).delete()
 
