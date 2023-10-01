ed45e9490e7363c48d4bedbad9db435e4b5aee43
SharonMai <ppp300a@gmail.com>
2023-10-01 23:34:31 +0800

    :memo: docs: Align docstrings with Google Python style guide for function depart_add

--- dept_app/views/depart.py
+++ dept_app/views/depart.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 
 from dept_app import models
-from dept_app.utils.pagination import Pagination
 
 
 def depart_list(request):
-    """部門列表"""
+    """
+    展示部門列表
+    """
 
     queryset = models.Department.objects.all()
 
@@ -18,7 +19,20 @@ def depart_list(request):
 
 
 def depart_add(request):
-    """新增部門列表"""
+    """
+    新增部門列表。
+
+    Args:
+        request (HttpRequest): Django的HTTP請求對象。
+
+    Returns:
+        HttpResponse: 基於請求類型，返回相應的HTML頁面或重定向。
+
+    Steps:
+        1. 檢查請求類型（GET或POST）。
+            1.1 如果是GET請求，返回空的新增部門表單。
+            1.2 如果是POST請求，添加新部門。
+    """
     if request.method == "GET":
         return render(request, 'depart_add.html',{"page_title": "Department Add"})
 
