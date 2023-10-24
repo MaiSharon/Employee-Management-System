74b5d99d601ee3aca41e84a5d39ff7cdebf77e3d
SharonMai <ppp300a@gmail.com>
2023-10-25 05:00:38 +0800

    :sparkles: feature: Add HTTPS control and email template

--- dept_app/utils/email_utils.py
+++ dept_app/utils/email_utils.py
@@ -1,5 +1,6 @@
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 import logging
@@ -21,13 +22,25 @@ def send_email_token(request, admin):
         # 獲取當前網站的域名
         current_site = get_current_site(request).domain
 
-        # 郵件主題
-        mail_subject = 'Activate your account.'
+
+        from django.conf import settings
+
+        protocol = 'https' if settings.USE_HTTPS else 'http'
 
         # 生成有加密的驗證連結
-        verification_link = f'http://{current_site}/verify/{urlsafe_base64_encode(force_bytes(admin.email_token))}'
+        verification_link = f'{protocol}://{current_site}/verify/{urlsafe_base64_encode(force_bytes(admin.email_token))}'
+        logo_link = f'{protocol}://{current_site}/static//logo-64.png'
+        content = {
+            'verification_link': verification_link,
+            'logo_link': logo_link
+        }
 
-        # 郵件內容
+        # 使用HTML模板生成郵件內容
+        html_message = render_to_string('email.html', content)
+
+
+        # 郵件主題和純文本內容
+        mail_subject = 'Quacqkersort.be - Email Verification'
         message = f'Please click on the link to confirm your email:\n{verification_link}'
 
         # 發送郵件
@@ -36,7 +49,8 @@ def send_email_token(request, admin):
             message,  # 內容
             'ppp300a@gmail.com',  # 發件人
             [admin.email],  # 收件人列表
+            html_message=html_message
         )
-        logger.info("OK sent verify email to:[REDACTED]")
+        logger.info('OK sent verify email to:[REDACTED]')
     except Exception as e:
-        logger.error(f"Failed to send verify email: {str(e)}")
+        logger.error(f'Failed to send verify email: {str(e)}')
