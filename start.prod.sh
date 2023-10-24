f3de72e49fc807a5efa8852a4ac421260f4309a5
SharonMai <ppp300a@gmail.com>
2023-10-25 04:42:17 +0800

    :recycle: refactor: Optimize logs and use Daphne server

--- start.prod.sh
+++ start.prod.sh
@@ -2,25 +2,21 @@
 set -e
 
 # 編譯 Django 消息
+echo "Compiling Django messages..."
 django-admin compilemessages
 
-
-# 嘗試使用 Django 的 manage.py shell 連接到數據庫並檢查 db_mai 數據庫
-echo "=== Attempting to connect to the database ==="
+# 嘗試連接到數據庫
+echo "Attempting to connect to the database..."
 until echo "from django.db import connection; cursor = connection.cursor(); cursor.execute('SHOW DATABASES'); print('db_mai' in [row[0] for row in cursor.fetchall()])" | python manage.py shell | grep "True"; do
     echo "Waiting for the database 'db_mai' to start"
     sleep 5
 done
-echo "Database 'db_mai' connection successful"
+echo "Successfully connected to the database 'db_mai'"
 
 # 執行數據庫遷移
-echo "=== Running database migrations ==="
-#python manage.py makemigrations
+echo "Running database migrations..."
 python manage.py migrate
 
-# 創建管理員用戶
-echo "=== Creating admin user if not exists ==="
-python create_admin.py
-
-# 使用 uWSGI 運行 Django 應用
-exec uwsgi --ini /data/prj_dept/uwsgi.ini
+# 使用 Daphne 運行 Django 應用
+echo "Starting Daphne server..."
+exec daphne -b 0.0.0.0 -p 8000 prj_dept.asgi:application
