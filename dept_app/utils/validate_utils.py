a62a353b597ad4c94fc3001439c0670e89fd4c01
SharonMai <ppp300a@gmail.com>
2023-10-25 04:44:29 +0800

    :bug: fix: Only input para can use

--- dept_app/utils/validate_utils.py
+++ dept_app/utils/validate_utils.py
@@ -21,21 +21,24 @@ def validate_search(search_input):
         str: 當輸入通過驗證->返回原始輸入字符串；否->返回空字符串。
 
     """
-    # 記錄調用此函數的文件和行號
-    caller_frame = inspect.stack()[1]
-    filename = caller_frame.filename.split("\\")[-1]  # 取得文件名，不包括路徑
-    lineno = caller_frame.lineno  # 取得行號
-    file_info = f"[file_info:{filename}:{lineno}]"
+    if search_input:
+        # 記錄調用此函數的文件和行號
+        caller_frame = inspect.stack()[1]
+        filename = caller_frame.filename.split("\\")[-1]  # 取得文件名，不包括路徑
+        lineno = caller_frame.lineno  # 取得行號
+        file_info = f"[file_info:{filename}:{lineno}]"
 
-    # 驗證字符串不可超過20個
-    if len(search_input) > 20:
-        logger.warning(f'{file_info}- Input too long: "{search_input}"')
-        return ''
-    # 驗證字符串只允許大小寫字母、數字、下畫線'_'和點'.'
-    elif not re.match("^[a-zA-Z0-9_.]+$", search_input):
-        logger.warning(f'{file_info}- Invalid text: "{search_input}"')
-        return ''
-    # 驗證通過
+        # 驗證字符串不可超過20個
+        if len(search_input) > 20:
+            logger.warning(f'{file_info}- Input too long: "{search_input}"')
+            return ''
+        # 驗證字符串只允許大小寫字母、數字、下畫線'_'和點'.'
+        elif not re.match("^[a-zA-Z0-9_.]+$", search_input):
+            logger.warning(f'{file_info}- Invalid text: "{search_input}"')
+            return ''
+        # 驗證通過
+        else:
+            logger.info(f'{file_info}- Search: "{search_input}"')
+            return search_input
     else:
-        logger.info(f'{file_info}- Search: "{search_input}"')
-        return search_input
+        return ''
