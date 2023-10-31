a333657d02ba4e3df21bffb3621a52ef1a190eab
SharonMai <ppp300a@gmail.com>
2023-10-31 15:42:55 +0800

    :sparkles: feature: Add backup cycles time to aws s3

---
+++ mysql8-backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 加載.env文件
+source .env
+
+# 定義其他變數
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+BACKUP_FILE="backup_$TIMESTAMP.sql"
+
+# 執行備份
+docker exec -T $CONTAINER_NAME /usr/bin/mysqldump -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_NAME > $BACKUP_FILE
+
+# 上傳到S3
+aws s3 cp $BACKUP_FILE s3://$S3_BUCKET/$BACKUP_FILE
+
+# 刪除本地備份
+rm -f $BACKUP_FILE
