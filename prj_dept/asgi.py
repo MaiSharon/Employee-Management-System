e385df0cc7e3b8bbc83de397a955db0ddd3900b4
SharonMai <ppp300a@gmail.com>
2023-11-07 13:58:56 +0800

    :bug: bug:Fix import sort and doc

--- prj_dept/asgi.py
+++ prj_dept/asgi.py
@@ -1,13 +1,21 @@
+"""
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings') 應該在其他所有導入之前。
+確保這一行出現在您導入任何其他 Django 模塊之前。
+有助於避免 AppRegistryNotReady 錯誤
+"""
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from . import routing
-from prj_dept.middleware_for_asgi import CustomAuthMiddleware  # 確保這個import路徑是正確的
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prj_dept.setting')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prj_dept.settings')
 
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from . import routing
+from .middleware_for_asgi import CustomAuthMiddleware  # 確保這個import路徑是正確的
+
+
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
