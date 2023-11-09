cc482919f99ea3776cb23d57a29fec58632b3f7d
SharonMai <ppp300a@gmail.com>
2023-11-09 17:36:23 +0800

    :bug: fix: Fix syntax

--- mysql8-backup.sh
+++ mysql8-backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 切換到腳本所在目錄
+cd /home/ubuntu/projects/dept_manage/prj_dept/
+
 # 加載.env文件
 source .env.prod
 
@@ -8,10 +11,10 @@ TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 BACKUP_FILE="backup_$TIMESTAMP.sql"
 
 # 執行備份
-docker exec -T $CONTAINER_NAME /usr/bin/mysqldump -u $MYSQL_USER --password=$MYSQL_PASSWORD $MYSQL_NAME > $BACKUP_FILE
+sudo docker compose -f docker-compose.prod.yml exec ${DOCKER_SERVICE_NAME} /usr/bin/mysqldump -uroot -p${MYSQL_ROOT_PASSWORD} ${MYSQL_NAME} > ${BACKUP_FILE}
 
 # 上傳到S3
-aws s3 cp $BACKUP_FILE s3://$S3_BUCKET/$BACKUP_FILE
+aws s3 cp $BACKUP_FILE s3://${S3_BUCKET}/${S3_BUCKET_FOLDER}/
 
 # 刪除本地備份
 rm -f $BACKUP_FILE
