d7a1a6885bf2906c461e84a50ec92ebbf404cc92
SharonMai <ppp300a@gmail.com>
2023-11-09 17:41:58 +0800

    :lock: feature: Add decorator to limit request methods in FBV.

--- dept_app/views/mobile.py
+++ dept_app/views/mobile.py
@@ -1,45 +1,36 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.decorators.http import require_POST, require_http_methods, require_GET
 
 from dept_app import models
 from dept_app.utils.form import MobileEditModelForm, MobileModelForm
+from dept_app.utils.pagination import Pagination
+from dept_app.utils.validate_utils import validate_search
 
-
+@require_GET
 def mobile_list(request):
+    # 接收用戶輸入搜尋內容
+    search_input = request.GET.get('search', '').strip()  # 獲取 'search' 參數的值，若無則為空字串。去除頭尾空格
+    # 驗證搜尋輸入內容
+    is_valid_search_input = validate_search(search_input)
 
-    search_dict = {}
-    search = request.GET.get("search", "")  # 預設為空字串，讓input不出現None保持乾淨空的
+    # 搜尋或返回所有數據
+    all_queryset_or_search_result = models.MobileNum.objects.filter(mobile__icontains=is_valid_search_input)
 
-    #
-    if search:
-        search_dict["Mobiles__contains"] = search  # 字典鍵值的值的新增方式
-
-    # 分頁組件化
-    from dept_app.utils.pagination import Pagination
-
-    # 把調數據傳給分頁組件的類
-    queryset = models.MobileNum.objects.filter(**search_dict).order_by("-status")
-
-    if not queryset:
-        print(search_dict, queryset)
-        queryset = models.MobileNum.objects.all().order_by("-status")
-
-    page_object = Pagination(request, queryset)
-
-    page = page_object.page
-
+    # 分頁處理
+    paginator = Pagination(request, queryset=all_queryset_or_search_result, page_size=5)
 
     context = {
-        "search": search,
-        "page": page,
+        'search': is_valid_search_input,
+        'mobiles': paginator.page_queryset,  # 分完頁的數據
+        'page_string': paginator.generate_html(),  # 頁碼
+        'page_title': 'Mobiles'
 
-        "queryset": page_object.page_queryset,  # 分完頁的數據條
-        "page_string": page_object.generate_html(), # 頁碼
-        "page_title": "Mobiles"
     }
 
     return render(request, "mobile_list.html", context)
 
-
+@require_http_methods(["GET", "POST"])
 def mobile_add(request):
     if request.method == "GET":
         form = MobileModelForm()
@@ -53,7 +44,7 @@ def mobile_add(request):
     return render(request, "mobile_add.html", {"form": form, "page_title": "Mobile Add"})
 
 
-
+@require_http_methods(['GET', 'POST'])
 def mobile_edit(request, nid):
 
     row_object = models.MobileNum.objects.filter(id=nid).first()
@@ -65,10 +56,11 @@ def mobile_edit(request, nid):
     if form.is_valid():
         # 如果校驗成功，保存到數據庫
         form.save()
-        return redirect("/mobile/list/")
+        return redirect('mobile_list')
     return render(request, "mobile_edit.html", {"form": form, "page_title": "Mobile Edit"})
 
-
+@require_POST
 def mobile_delete(request, nid):
     models.MobileNum.objects.filter(id=nid).delete()
-    return redirect("/mobile/list/")
+    return redirect('mobile_list')
+
