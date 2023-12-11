1f5b2101ece5ea07b49b5fd5841d68ac8e6551f2
SharonMai <ppp300a@gmail.com>
2023-12-11 21:18:46 +0800

    :art: refactor: Update docstring

--- dept_app/middleware/auth.py
+++ dept_app/middleware/auth.py
@@ -1,37 +1,56 @@
 import re
 import logging
 
-from django.utils.deprecation import MiddlewareMixin
 from django.shortcuts import redirect
+from django.utils.deprecation import MiddlewareMixin
 
 logger = logging.getLogger(__name__)
 
-
 class AuthMiddleware(MiddlewareMixin):
-    """登入校驗"""
+    """
+    用戶登入驗證中間件，負責處理用戶登入狀態和訪問控制。
+
+    Attributes:
+        exclude_paths (list[str]): 不需要登入即可訪問的 URL 列表
+
+    Methods:
+        process_request: 處理 HTTP 請求以驗證用戶登入狀態和管理可訪問的路徑
+    """
+    exclude_paths = [
+        '/swagger/',
+        '/sentry-debug/',
+        '/re-verify/',
+        '/login/',
+        '/register/',
+        '/image/code/',
+        r'^/verify/.+/$',
+        '/api/task-choices/'
+    ]
     def process_request(self, request):
+        """
+        處理進入的 HTTP 請求。負責驗證用戶登入狀態、管理可訪問的路徑。
 
-        # 如果沒有返回值(返回None)，繼續往後走
-        # 如果有返回值 HttpResponse, render, redirect，則直接在此中間件中斷不繼續向後執行
+        Steps:
+        1. 檢查當前訪問路徑是否在排除路徑列表中。
+        2. 若在排除路徑列表中，允許請求繼續。
+        3. 若不在排除路徑列表中，則檢查用戶的 session 是否包含登入信息。
+        4. 若用戶 session 中無登入信息，則重定向到登入頁面。
 
-        exclude_paths = [
-            '/swagger/',
-            '/sentry-debug/',
-            '/re-verify/',
-            '/login/',
-            '/register/',
-            '/image/code/',
-            r'^/verify/.+/$',
-            '/api/task-choices/'
-        ]
+        Args:
+            request: Django 的 HttpRequest 對象，包含請求的詳細信息。
 
-        exclude_paths_regex = [re.compile(pattern) for pattern in exclude_paths]
+        Returns:
+            HttpResponse or None: 若用戶未登入且訪問非排除路徑，則重定向到登入頁面。否則，返回 None 以繼續處理請求。
+        """
+        # 編譯排除路徑的正則表達式列表
+        exclude_paths_regex = [re.compile(pattern) for pattern in self.exclude_paths]
 
-        # 加入正則表達式以匹配動態路由
+        # 檢查訪問路徑是否在排除路徑中
         if request.path_info.startswith('/__debug__/') or any(
                 pattern.match(request.path_info) for pattern in exclude_paths_regex):
             return
-        # 檢查用戶session，如果用戶未登錄且訪問的不是排除路徑，則重定向到登錄頁面
+
+        # 檢查用戶的 session 是否包含登入信息
         info_dic = request.session.get('info')
         if not info_dic and not any(pattern.match(request.path_info) for pattern in exclude_paths_regex):
             logger.info(f"Non-verify user from {request.path_info} to login page")
