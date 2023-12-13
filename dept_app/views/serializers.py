f088815fa4d7e13090a33246d9fe90af577061be
SharonMai <ppp300a@gmail.com>
2023-12-14 03:21:23 +0800

    Revert "Revert ":sparkles: feature: Add validation""
    This reverts commit 24b567c91e352820a9cde3ac732a0bad318f6082.

--- dept_app/views/serializers.py
+++ dept_app/views/serializers.py
@@ -10,12 +10,30 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_level_display(self, obj):
         """
-        顯示 Task 級別的選項文字。
+        顯示 Task任務優先級的選項文字。
 
         Returns:
-            list of tuple: Task 級別的選項列表。每個元組格式為 (數字代碼, 文字描述)。
+            list of tuple: Task任務優先級的選項文字列表。每個元組格式為 (數字代碼, 文字描述)。
 
         Example:
             返回值示例：[(1, "重要"), (2, "普通"), (3, "閒置")]
         """
         return obj.get_level_display()
+
+    def validate_title(self, value):
+        # 驗證標題長度
+        if len(value) < 3:
+            raise serializers.ValidationError("標題太短")
+        return value
+
+    def validate_detail(self, value):
+        # 驗證詳細訊息的長度
+        if len(value) > 50:
+            raise serializers.ValidationError("詳細訊息太長")
+        return value
+
+    def validate(self, data):
+        # 跨欄位的綜合驗證
+        if '請款' in data['title'] and data['level'] != 1:
+            raise serializers.ValidationError("請款的任務級別必須為 '重要'")
+        return data
